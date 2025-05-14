c2ec13cb899e46cbdee96c2ec79ae63ac9b36858
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-05-14 14:25:23 +0530

    feat - health check added

---
+++ src/controller/health.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { checkHealth, JsonResponseToText } from "../utils/healthCheck";
+
+export const healthCheckAPI = async (req: Request, res: Response) => {
+  try {
+    // Define the current service and its dependencies
+    const currentService = {
+      name: "Seller Mock Engine",
+      url:
+        process.env.DEPLOYED_URL + "/health-self" ||
+        "http://localhost:7202/health-self", // check current service by session api route
+    };
+
+    const dependencyServices = [
+      {
+        name: "Protocol Server Engine",
+        url:
+          process.env.PROTOCOL_SERVER_BASE_URL + "health-self" ||
+          "http://localhost:7201/health-self",
+      },
+      {
+        name: "Mock UI",
+        url: process.env.MOCK_UI_URL || "http://localhost:3000",
+      },
+    ];
+
+    const healthStatus = await checkHealth(currentService, dependencyServices);
+    const textResponse = JsonResponseToText(healthStatus);
+    res.status(healthStatus.statusCode).send(textResponse);
+  } catch (error) {
+    console.error("Error in healthCheckAPI:", error);
+    res.status(500).send({
+      message: "An error occurred while processing the health check.",
+      error: error,
+    });
+  }
+};
